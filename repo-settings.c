f2bd766e0af866a9d5a69d6f86ece7fca9d36380
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-08-04 00:02:04 +0200

    fixup??? Enable the built-in FSMonitor as an experimental feature
    Required by a new FSMonitor iteration.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- repo-settings.c
+++ repo-settings.c
@@ -119,9 +119,9 @@ void prepare_repo_settings(struct repository *r)
 
 	if (!repo_config_get_bool(r, "feature.experimental", &value) && value) {
 		UPDATE_DEFAULT_BOOL(r->settings.fetch_negotiation_algorithm, FETCH_NEGOTIATION_SKIPPING);
-		if (feature_many_files && fsmonitor_ipc__is_supported())
-			UPDATE_DEFAULT_BOOL(r->settings.use_builtin_fsmonitor,
-					    1);
+		if (r->settings.fsmonitor_mode == FSMONITOR_MODE_UNSET &&
+		    feature_many_files && fsmonitor_ipc__is_supported())
+			r->settings.fsmonitor_mode = FSMONITOR_MODE_IPC;
 	}
 
 	/* Hack for test programs like test-dump-untracked-cache */
